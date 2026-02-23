1dfc7652e61a22c9b00671980944bb3c14bf567b
davidcllm <davcm24@hotmail.com>
2026-02-22 19:39:45 -0600

    Prueba 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        IComponent song1 = new Song("Soñé", "Zoé");
+        IComponent song2 = new Song("Hey Jude", "The Beatles");
+        IComponent song3 = new Song("Remember the time", "Michael Jackson");
+
+        Playlist rockPlaylist = new Playlist("Playlist de rock");
+        Playlist favorites = new Playlist("Favoritas");
+
+        rockPlaylist.add(song1);
+        rockPlaylist.add(song2);
+
+        favorites.add(rockPlaylist); //Composite
+        favorites.add(song3);
+
+        favorites.play();
+
+        System.out.println("\n--- Cambiando velocidad ---\n");
+
+        favorites.setPlaybackSpeed(1.5f);
+
+        System.out.println("\n--- Reproduciendo nuevamente ---\n");
+
+        favorites.play();
     }
 }
